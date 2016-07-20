d0616c88f8549f3b45a2fbb0eaebcfea1c009f7c
JohannaOssa <johannaossa10@gmail.com>
2016-07-19 20:49:17 -0500

    anexo gema bootstrap.sass, anexo campos formulario user y controlador para vehicles

---
+++ app/controllers/vehicles_controller.rb
@@ -0,0 +1,24 @@
+class VehiclesController <ApplicationController
+  def new
+    @user = current_user
+    @vehicle = Vehicle.new
+  end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    @vehicle.user_id = current_user
+    if @vehicle.save
+      flash[:success] ='Vehicle registered successfully.'
+      redirect_to_user_vehicles_path(current_user)
+    else
+      flash[:error] = @vehicle.errors.full_messages.join(',')
+      render 'new'
+    end
+  end
+
+  def vehicle_params
+    params.require(:vehicle).permit(:user_id, :model, :year, :vin, :id)
+  end
+  private :vehicle_params
+
+end
