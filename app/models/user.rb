815d8f6925700446431460dbd2a9c5f62a9133d0
JohannaOssa <johannaossa10@gmail.com>
2016-07-22 18:33:03 -0500

    agregar validaciones a User.
    agregar validaciones a User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :first_name, presense: true, format: { with: /\A[A-Z]+\z/ }, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :vehicles
