7c2fdc37d90ed9e5b38ca6792e2967aef480b585
JohannaOssa <johannaossa10@gmail.com>
2016-07-28 19:28:37 -0500

    agregue index y new para Locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,26 @@
-class LocationsController <ApplicationController
- def index
-   @locations = Location.all
-   flash[:error] = 'No locations registered.' if @locations.empty?
+class LocationsController < ApplicationController
+  def index
+    @locations = Location.all
+    flash[:error] = 'No locations registered.' if @locations.empty?
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      flash[:success] = 'Location registered successfully.'
+      redirect_to locations_path
+    else
+      flash[:error] = @location.errors.full_messages.join(',')
+      render 'new'
+    end
+  end
+
+  def location_params
+    params.require(:location).permit(:address, :latitude, :longitude, :name, :description)
+  end
+  private :location_params
 end
