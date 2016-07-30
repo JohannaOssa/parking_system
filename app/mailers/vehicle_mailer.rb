5753830e14206eafcc02bfa16d1ec3496ab04f72
JohannaOssa <johannaossa10@gmail.com>
2016-07-29 20:22:04 -0500

    ActionMailer

---
+++ app/mailers/vehicle_mailer.rb
@@ -0,0 +1,13 @@
+class VehicleMailer < ApplicationMailer
+default from: 'notifications@parkingsystem.com'
+
+ def new_vehicle_email(user)
+   @user = user
+   if Rails.env == 'development'
+     @url = "http://localhost:3000/users/#{@user.id}/vehicles"
+   elsif Rails.env == 'production'
+     @url = "http://johaps.herokuapp.com/users/#{@user.id}/vehicles"
+   end
+   mail(to: @user.email, subject: 'New Vehicle Registered.')
+ end
+end
